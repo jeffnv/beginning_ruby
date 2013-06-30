ba951cd7c615eadf2069921820fb9d9e263ab945
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-30 11:02:54 -0700

    finished basic section, now on to the 'advanced features'..oooh

--- ch4_text_analyzer/txtr.rb
+++ ch4_text_analyzer/txtr.rb
@@ -29,16 +29,33 @@ class Txtr
     #self.to_s.scan(/\w+/).count
     self.to_s.split.length
   end
+  
+  def count_sentences
+    self.to_s.split(/\.|\?|!/).count
+  end
+  
+  def count_paragraphs
+    self.to_s.split(/\n\n/).count
+  end
+  
+  def avg_words_per_sentence
+    count_words.to_f / count_sentences.to_f
+  end
+  
+  def avg_sentence_per_pg
+    count_sentences.to_f / count_paragraphs.to_f
+  end
     
   def analyze
     puts "text has #{count_lines} lines."
     puts "text is #{count_all_symbols} symbols."
     puts "text has #{count_all_characters} characters (excluding white spaces)."
     puts "text has #{count_words} words."
-    #puts "text is #{lines.join('\n').scan(/\w+/).join('').length} characters without whitespaces"
-    #puts "text is #{lines.join('\n').gsub(/\s+/,'').length} characters without whitespaces (gsub)"
+    puts "text has #{count_sentences} sentences."
+    puts "text has #{count_paragraphs} paragraphs."
+    puts "text has an average of #{avg_words_per_sentence.round(2)} words per sentence."
+    puts "text has an average of #{avg_sentence_per_pg.round(2)} sentences per paragraph."
   end
 end
 
-t = Txtr.new("oliver.txt")
-Txtr.new("oliver.txt").analyze
+t = Txtr.new("oliver.txt").analyze
